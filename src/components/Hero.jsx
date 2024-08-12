f3f46a5e1e70d5b81d99927e407ad4ff92f319ed
mlshere <mlsherezade@gmail.com>
2024-08-12 11:33:56 +0200

    Add interactive button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { motion } from 'framer-motion';
 import { styles } from '../styles'; 
-import { CubeComponent } from './canvas';
+import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
@@ -19,8 +19,26 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <div className="mt-50">
-        <CubeComponent />
+      <div>
+        <ComputersCanvas />
+      
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center">
+        <a href="#about" >
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div 
+              animate={{
+                y: [0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className="w-3 h-3 bg-secondary rounded-full mb-1"
+            />
+          </div>
+        </a>
+      </div>
       </div>
     </section>
   )
