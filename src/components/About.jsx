562d9f994bb55cdefe7c7833db1cf328e7173450
mlshere <mlsherezade@gmail.com>
2024-08-12 16:18:40 +0200

    Add about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,68 @@
 import React from 'react'
+import { services } from '../constants';
+import { motion } from 'framer-motion';
+import { Tilt } from 'react-tilt';
+import { fadeIn, textVariant } from '../utils/motion';
+import { styles } from '../styles';
+import { SectionWrapper } from '../hoc';
 
-const About = () => {
+const ServiceCard = ({ index, title, description, icon }) => {
   return (
-    <div>About</div>
+    <Tilt>
+    
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className=""
+      >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450
+          }}
+        className="bg-black-gradient ">      
+
+        <div key={index} className="min-h-[250px] border-2 border-white p-4 bg-[#1e1e1e] rounded-lg">
+              <div className="flex items-center mb-4">
+                <div className="text-white">
+                  {icon}
+                </div>
+                <h4 className="text-white text-xl font-semibold">
+                  <span className="relative inline-block">
+                    {title}
+                    <span className="absolute bottom-0 left-0 w-full h-1 bg-[#ee7c53] rounded-full"></span>
+                  </span>
+                </h4>
+              </div>
+              <div className="flex-grow flex items-center justify-center">
+                <p className="text-white text-center">{description}</p>
+              </div>
+            </div>
+      </div>
+      </motion.div>
+
+    </Tilt>
   )
 }
 
-export default About
+const About = () => {
+  return (
+    <>
+    <section className="">
+        <motion.div>
+          <h2 className={styles.sectionHeadText}>My Expertise</h2>
+        </motion.div>
+        <div className="flex flex-col items-center justify-center w-full h-full px-8 py-12">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 mx-16">
+          {services.map((service, index) => (
+            <ServiceCard key={service.title} index={index} {...service} />
+          ))}
+        </div>
+      </div>
+    </section>
+    </>
+  )
+}
+
+export default SectionWrapper(About, "about")
+
