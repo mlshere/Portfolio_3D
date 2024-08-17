de6f2ff8c997a42811eb1b53d6eb031ff4db5148
mlshere <mlsherezade@gmail.com>
2024-08-17 14:47:01 +0200

    Delete vite import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import { BrowserRouter } from 'react-router-dom';
 import { Expertise, Contact, Project, Hero, Navbar, Works, StarsCanvas, About} from './components';
 
