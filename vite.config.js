c1b20a68ef73f049f3a000d0df52fa62c5ef629f
mlshere <mlsherezade@gmail.com>
2024-08-17 14:42:07 +0200

    Update Warning limits

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 5000, // increase the limit (default is 500 KB)
+  },
 })
+
+
