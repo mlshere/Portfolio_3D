1bdba8aceb3fd3a2618889260a0723683f0cf71b
mlshere <mlsherezade@gmail.com>
2024-08-26 18:10:58 +0200

    Revert changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,12 +13,12 @@ const Hero = () => {
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
         >
             <div className="flex flex-col justify-center items-center mt-5">
-              <div className="w-5 h-5 rounded-full bg-[#ee7c53]" /> 
-              <div className="w-1 sm:h-80 h-40 orange-gradient ml-2" />
+              <div className="w-5 h-5 rounded-full bg-[#ac829e]" /> 
+              <div className="w-1 sm:h-80 h-40 purple-gradient" />
             </div>
 
             <div>
-              <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#ee7c53]'>Sherezade</span></h1>
+              <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#ac829e]'>Sherezade</span></h1>
               <p className={`${styles.heroSubText} mt-2 text-white-100`}>
               web developer focused on crafting beautiful, functional, and user-centered digital experiences.
               </p>
