38909ca9c3488a62c2b33563f5ed66795b8b8694
mlshere <mlsherezade@gmail.com>
2025-02-07 20:57:56 +0100

    Update project images

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,7 +6,7 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-
+import reddit from "./reddit.png";
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
@@ -20,17 +20,16 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-
+import spotifind from "./spotifind.png";
 import berlin from "./company/berlin.png";
 import code from "./company/code.svg";
 import groningen from "./company/groningen.png";
 import hamburg from "./company/hamburg.png";
 import madrid from "./company/madrid.png";
-
 import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png"
+import tripguide from "./tripguide.png";
 import lobelokal from "./lobelokal.png";
+import portfolio from "./portfolio.png";
 
 export {
   logo,
@@ -60,7 +59,9 @@ export {
   hamburg,
   madrid,
   carrent,
-  jobit,
+  spotifind,
   tripguide,
-  lobelokal
+  lobelokal, 
+  reddit,
+  portfolio
 };
